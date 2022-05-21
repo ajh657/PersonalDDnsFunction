c3096c0cfae613255b70e1209f63902bbd17a5b6
ajh657 <aahoo01@gmail.com>
2022-05-21 18:50:46 +0300

    curl fix

--- src/updateDNS.sh
+++ src/updateDNS.sh
@@ -7,5 +7,5 @@ FUNCTIONURL=""
 
 if [ "$NEWIP" != "$OLDIP" ]; then
     echo $NEWIP > currentIP.txt
-    curl -s -X POST -H "Content-Type: application/json" -d "{\"ip\":\"$NEWIP\"}" $FUNCTIONURL
+    curl -s -X POST -d "$NEWIP" $FUNCTIONURL
 fi
